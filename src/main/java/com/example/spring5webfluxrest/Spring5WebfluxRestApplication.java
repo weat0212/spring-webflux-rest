5d5a643b198b3579b77937c06b57dd7d6eaaf478
weat0212 <weat0212@gmail.com>
2021-04-09 13:25:31 +0800

    first commit

---
+++ src/main/java/com/example/spring5webfluxrest/Spring5WebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5WebfluxRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
